2dc99ab97408136669e04a0eb51dcee777a9b12f
Harsh Gujrati <harshgujrati2003@gmail.com>
2023-05-15 13:26:50 +0530

    internal cards added

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 import "./App.css";
+import ITraining from "./components/Courses/ITraining";
+import CampusTraining from "./components/Courses/CampusTraining";
+import MockInterview from "./components/Courses/MockInterview";
 
 function App() {
     return (
@@ -23,6 +26,9 @@ function App() {
                         <Route exact path="/courses" element={<CoursesNew />} />
                         <Route exact path="/about" element={<About />} />
                         <Route exact path="/contact" element={<Contact />} />
+                        <Route exact path="/itraining" element={<ITraining />} />
+                        <Route exact path="/campustraining" element={<CampusTraining />} />
+                        <Route exact path="/mockinterview" element={<MockInterview />} />
                     </Routes>
                 </div>
             </Router>
