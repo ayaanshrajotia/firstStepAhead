bf6da17d256d022f7f90af20c4e5a0444c1ead12
Ayaansh Rajotia <86037577+ayaanshrajotia@users.noreply.github.com>
2023-05-24 23:32:20 +0530

    completed contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "../css/Contact.css";
 import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
 
 export default function Contact() {
     return (
@@ -8,64 +9,78 @@ export default function Contact() {
             <Header image="https://websitedemos.net/online-courses-02/wp-content/uploads/sites/542/2020/02/bg-01-free-img.jpg">
                 <div className="header__container">
                     <h1>Contact us</h1>
-                    {/* <p>
+                    <p>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                         Ut elit tellus, luctus nec ullamcorper mattis, pulvinar
                         dapibus leo.
-                    </p> */}
+                    </p>
                 </div>
             </Header>
             <div className="contact">
                 <div className="contact__content-form">
                     <h2>Course Registration Form</h2>
                     <form>
-                        <div className="contact__content-form-field">
-                            <label htmlFor="name">Name</label>
-                            <input type="text" name="name" id="name" />
-                        </div>
-                        <div className="contact__content-form-field">
-                            <label htmlFor="college">College</label>
-                            <input type="text" name="college" id="college" />
-                        </div>
-                        <div className="contact__content-form-field">
-                            <label htmlFor="branch">Branch</label>
-                            <input type="text" name="branch" id="branch" />
-                        </div>
-                        <div className="contact__content-form-field">
-                            <label htmlFor="mobile">Mobile</label>
-                            <input type="text" name="mobile" id="mobile" />
-                        </div>
-                        <div className="contact__content-form-field">
-                            <label htmlFor="email">E-Mail</label>
-                            <input type="email" name="email" id="email" />
-                        </div>
-                        <div className="contact__content-form-field">
-                            <label htmlFor="course">Course</label>
-                            <select name="course" id="course">
-                                <option value="c">Select Course</option>
-                                <option value="c">C</option>
-                                <option value="c++">C++</option>
-                                <option value="java">Java</option>
-                                <option value="python">Python</option>
-                            </select>
+                        <div className="form__inner">
+                            <div className="form__inner--col">
+                                <div className="contact__content-form-field">
+                                    <label htmlFor="name">Name</label>
+                                    <input type="text" name="name" id="name" />
+                                </div>
+                                <div className="contact__content-form-field">
+                                    <label htmlFor="college">College</label>
+                                    <input
+                                        type="text"
+                                        name="college"
+                                        id="college"
+                                    />
+                                </div>
+                                <div className="contact__content-form-field">
+                                    <label htmlFor="branch">Branch</label>
+                                    <input
+                                        type="text"
+                                        name="branch"
+                                        id="branch"
+                                    />
+                                </div>
+                            </div>
+                            <div className="form__inner--col">
+                                <div className="contact__content-form-field">
+                                    <label htmlFor="mobile">Mobile</label>
+                                    <input
+                                        type="text"
+                                        name="mobile"
+                                        id="mobile"
+                                    />
+                                </div>
+                                <div className="contact__content-form-field">
+                                    <label htmlFor="email">E-Mail</label>
+                                    <input
+                                        type="email"
+                                        name="email"
+                                        id="email"
+                                    />
+                                </div>
+                                <div className="contact__content-form-field">
+                                    <label htmlFor="course">Course</label>
+                                    <select name="course" id="course">
+                                        <option value="c">Select Course</option>
+                                        <option value="c">C</option>
+                                        <option value="c++">C++</option>
+                                        <option value="java">Java</option>
+                                        <option value="python">Python</option>
+                                    </select>
+                                </div>
+                            </div>
                         </div>
 
                         <button type="submit">Submit Form</button>
                     </form>
                 </div>
-
-                {/* <div className="contact__content">
-                    <div className="contact__content-address">
-                        <h3>Find Address</h3>
+                <div className="contact__content-address">
+                    <h3>Find Address</h3>
+                    <div className="contact__content--address--inner">
                         <div className="contact__content-address-field">
                             <img
-                                style={{
-                                    border: "none",
-                                    padding: "none",
-                                    width: "30px",
-                                    height: "33px",
-                                    borderRadius: "0",
-                                }}
                                 src="./images/icons/register.svg"
                                 alt="register-icon"
                             />
@@ -85,20 +100,25 @@ export default function Contact() {
                                 src="./images/icons/call.svg"
                                 alt="register-icon"
                             />
-                            <p className="field-head">Mobile:</p>
-                            <p>{`(+91) 9993981781`}</p>
+                            <p>
+                                <span className="field-head">Mobile:</span>
+                                <br /> {`(+91) 9993981781`}
+                            </p>
                         </div>
                         <div className="contact__content-address-field">
                             <img
                                 src="./images/icons/email.svg"
                                 alt="register-icon"
                             />
-                            <p className="field-head">Email:</p>
-                            <p>training.firststep1@gmail.com</p>
+                            <p>
+                                <span className="field-head">Email:</span>{" "}
+                                <br /> training.firststep1@gmail.com
+                            </p>
                         </div>
                     </div>
-                </div> */}
+                </div>
             </div>
+            <Footer />
         </>
     );
 }
