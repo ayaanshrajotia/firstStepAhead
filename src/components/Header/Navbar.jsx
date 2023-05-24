5557b9d5082947c26e0226121a893deabc54ebf4
Harsh Gujrati <harshgujrati2003@gmail.com>
2023-05-24 12:42:32 +0530

    drop down

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -2,81 +2,93 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import "./Navbar.css";
 import { FiMenu } from "react-icons/fi";
-
+import { Link } from "react-router-dom";
 const Navbar = () => {
-    return (
-        <>
-            <div className="navigation-bar">
-                <img
-                    className="w-12 h-10"
-                    src="https://www.firststepahead.in/images/FSA_Logo.png"
-                    alt="navbar-logo"
-                />
+  return (
+    <>
+      <div className="navigation-bar">
+        <img
+          className="w-12 h-10"
+          src="https://www.firststepahead.in/images/FSA_Logo.png"
+          alt="navbar-logo"
+        />
 
-                <button className="nav-hamburger">
-                    <FiMenu />
-                </button>
-                <ul className="navbar-navigation">
-                    <li className="">
-                        <NavLink
-                            className={({ isActive }) =>
-                                isActive
-                                    ? "navbar-link-active navbar-link"
-                                    : "navbar-link"
-                            }
-                            to={"/"}
-                        >
-                            Home
-                        </NavLink>
-                    </li>
-                    <li className="w-32">
-                        <NavLink
-                            className={({ isActive }) =>
-                                isActive
-                                    ? "navbar-link-active navbar-link"
-                                    : "navbar-link"
-                            }
-                            to={"/about"}
-                        >
-                            About
-                        </NavLink>
-                    </li>
-                    <li className="w-32">
-                        <NavLink
-                            className={({ isActive }) =>
-                                isActive
-                                    ? "navbar-link-active navbar-link"
-                                    : "navbar-link"
-                            }
-                            to={"/courses"}
-                        >
-                            Our Courses
-                        </NavLink>
-                    </li>
-                    <li className="w-32">
-                        <NavLink
-                            className={({ isActive }) =>
-                                isActive
-                                    ? "navbar-link-active navbar-link"
-                                    : "navbar-link"
-                            }
-                            to={"/contact"}
-                        >
-                            Contact
-                        </NavLink>
-                    </li>
-                    <li>
-                        <a
-                            href="https://www.firststepahead.in/login.html"
-                            className="btn-login"
-                        >
-                            Login
-                        </a>
-                    </li>
-                </ul>
+        <button className="nav-hamburger">
+          <FiMenu />
+        </button>
+        <ul className="navbar-navigation">
+          <li className="">
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? "navbar-link-active navbar-link" : "navbar-link"
+              }
+              to={"/"}
+            >
+              Home
+            </NavLink>
+          </li>
+          <li className="w-32">
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? "navbar-link-active navbar-link" : "navbar-link"
+              }
+              to={"/about"}
+            >
+              About
+            </NavLink>
+          </li>
+          <li className="w-32">
+            <div className="dropdown">
+              <button
+                className="btn dropdown-toggle"
+                type="button"
+                id="dropdownMenu2"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Our Courses
+              </button>
+              <ul className="dropdown-menu" aria-labelledby="dropdownMenu2">
+                <li>
+                  <Link to="/courses/ittraining" className="dropdown-item p-3" type="button">
+                    IT Training
+                  </Link>
+                </li>
+                <li>
+                  <Link to={"/courses/campustraining"} className="dropdown-item p-3" type="button">
+                    Campus Training
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/courses/mockinterview" className="dropdown-item p-3" type="button">
+                    Mock Interview
+                  </Link>
+                </li>
+              </ul>
             </div>
-        </>
-    );
+          </li>
+          <li className="w-32">
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? "navbar-link-active navbar-link" : "navbar-link"
+              }
+              to={"/contact"}
+            >
+              Contact
+            </NavLink>
+          </li>
+          <li>
+            <a
+              href="https://www.firststepahead.in/login.html"
+              className="btn-login"
+            >
+              Login
+            </a>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 };
 
 export default Navbar;
